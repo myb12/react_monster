397f201e257233e82eaeb0db4cf2f47c0c62a5c6
myb12 <mohammadyasinbappy@outlook.com>
2021-08-25 02:12:45 +0600

    Hompage styles added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component{
     const filteredMonsters = monsters.filter(monster=>monster.name.toLowerCase().includes(searchFiled.toLowerCase()));
       return (
     <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters}/>
     </div>
